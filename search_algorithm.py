9482fa11c70988c35668234b56209decf9e8b7e7
aldiset <aldione249@gmail.com>
2023-06-06 21:22:01 +0700

    write the search algorithm code

---
+++ search_algorithm.py
@@ -0,0 +1,32 @@
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+def binary_search_recursive(arr, target, low, high):
+    if low > high:
+        return -1
+
+    mid = (low + high) // 2
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] < target:
+        return binary_search_recursive(arr, target, mid + 1, high)
+    else:
+        return binary_search_recursive(arr, target, low, mid - 1)
